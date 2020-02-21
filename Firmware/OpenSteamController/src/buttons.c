964f570c924f6598b49eab63958adeb3ac92aef2
returnzork <1>
2020-02-20 17:04:27 -0800

    Initial work on disabling buttons

--- Firmware/OpenSteamController/src/buttons.c
+++ Firmware/OpenSteamController/src/buttons.c
@@ -34,6 +34,9 @@
 #include "usb.h"
 #include "time.h"
 
+#include "eeprom_access.h"
+#include <stdlib.h>
+
 #include <stdio.h>
 
 #define GPIO_ANALOG_JOY_CLICK 1, 0
@@ -52,6 +55,27 @@
 #define GPIO_B_BTN 1, 22
 #define GPIO_X_BTN 1, 9
 #define GPIO_A_BTN 0, 17
+
+
+#define OPTIONS_OFFSET (0x100)
+int analogJoyClickEnabled = 1;
+int leftGripEnabled = 1;
+int steamButtonEnabled = 1;
+int leftTrackpadEnabled = 1;
+int leftTriggerEnabled = 1;
+int leftBumperEnabled = 1;
+int frontLeftEnabled = 1;
+int frontRightEnabled = 1;
+int rightTrackpadEnabled = 1;
+int yButtonEnabled = 1;
+int rightTriggerEnabled = 1;
+int rightBumperEnabled = 1;
+int rightGripEnabled = 1;
+int bButtonEnabled = 1;
+int xButtonEnabled = 1;
+int aButtonEnabled = 1;
+
+
 
 /**
  * Initialize GPIOs used to read button states.
@@ -91,118 +115,177 @@ void initButtons(void) {
 		IOCON_MODE_PULLUP, IOCON_FUNC0);
 	Chip_IOCON_PinMux(LPC_IOCON, GPIO_A_BTN, IOCON_HYS_EN | 
 		IOCON_MODE_PULLUP, IOCON_FUNC0);
+
+
+	void* ptr = malloc(sizeof(int));
+	for(int i = 0; i < 16; i++)
+	{
+		eepromRead(OPTIONS_OFFSET, ptr, 8);
+		switch((uint8_t)ptr) {
+		case 0:
+			analogJoyClickEnabled = 0;
+			break;
+		case 1:
+			leftGripEnabled = 0;
+			break;
+		case 2:
+			steamButtonEnabled = 0;
+			break;
+		case 3:
+			leftTrackpadEnabled = 0;
+			break;
+		case 4:
+			leftTriggerEnabled = 0;
+			break;
+		case 5:
+			leftBumperEnabled = 0;
+			break;
+		case 6:
+			frontLeftEnabled = 0;
+			break;
+		case 7:
+			frontRightEnabled = 0;
+			break;
+		case 8:
+			rightTrackpadEnabled = 0;
+			break;
+		case 9:
+			yButtonEnabled = 0;
+			break;
+		case 10:
+			rightTriggerEnabled = 0;
+			break;
+		case 11:
+			rightBumperEnabled = 0;
+			break;
+		case 12:
+			rightGripEnabled = 0;
+			break;
+		case 13:
+			bButtonEnabled = 0;
+			break;
+		case 14:
+			xButtonEnabled = 0;
+			break;
+		case 15:
+			aButtonEnabled = 0;
+			break;
+		}
+	}
+
+	free(ptr);
 }
 
 /**
  * \return true if Steam Button is being pressed. False otherwise.
  */
 int getSteamButtonState(void) {
-	return !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_STEAM_BTN);
+	return steamButtonEnabled && !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_STEAM_BTN);
 }
 
 /**
  * \return true if Front Left Arrow Button is being pressed. False otherwise.
  */
 int getFrontLeftButtonState(void) {
-	return !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_FRONT_L);
+	return frontLeftEnabled && !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_FRONT_L);
 }
 
 /**
  * \return true if Front Right Arrow Button is being pressed. False otherwise.
  */
 int getFrontRightButtonState(void) {
-	return !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_FRONT_R);
+	return frontRightEnabled && !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_FRONT_R);
 }
 
 /**
  * \return true if Analog Joystick Click Button is being pressed. False otherwise.
  */
 int getJoyClickState(void) {
-	return !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_ANALOG_JOY_CLICK);
+	return analogJoyClickEnabled && !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_ANALOG_JOY_CLICK);
 }
 
 /**
  * \return true if X Button is being pressed. False otherwise.
  */
 int getXButtonState(void) {
-	return !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_X_BTN);
+	return xButtonEnabled && !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_X_BTN);
 }
 
 /**
  * \return true if Y Button is being pressed. False otherwise.
  */
 int getYButtonState(void) {
-	return !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_Y_BTN);
+	return yButtonEnabled && !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_Y_BTN);
 }
 
 /**
  * \return true if B Button is being pressed. False otherwise.
  */
 int getBButtonState(void) {
-	return !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_B_BTN);
+	return bButtonEnabled && !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_B_BTN);
 }
 
 /**
  * \return true if A Button is being pressed. False otherwise.
  */
 int getAButtonState(void) {
-	return !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_A_BTN);
+	return aButtonEnabled && !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_A_BTN);
 }
 
 /**
  * \return true if Inner Right Grip Button is being pressed. False otherwise.
  */
 int getRightGripState(void) {
-	return !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_R_GRIP);
+	return rightGripEnabled && !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_R_GRIP);
 }
 
 /**
  * \return true if Inner Left Grip Button is being pressed. False otherwise.
  */
 int getLeftGripState(void) {
-	return !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_L_GRIP);
+	return leftGripEnabled && !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_L_GRIP);
 }
 
 /**
  * \return true if Right Trackpad Click Button is being pressed. False otherwise.
  */
 int getRightTrackpadClickState(void) {
-	return !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_R_TRACKPAD);
+	return rightTrackpadEnabled && !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_R_TRACKPAD);
 }
 
 /**
  * \return true if Left Trackpad Click Button is being pressed. False otherwise.
  */
 int getLeftTrackpadClickState(void) {
-	return !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_L_TRACKPAD);
+	return leftTrackpadEnabled && !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_L_TRACKPAD);
 }
 
 /**
  * \return true if Right Digital Trigger Button is being pressed. False otherwise.
  */
 int getRightTriggerState(void) {
-	return !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_R_TRIGGER);
+	return rightTriggerEnabled && !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_R_TRIGGER);
 }
 
 /**
  * \return true if Left Digital Trigger Button is being pressed. False otherwise.
  */
 int getLeftTriggerState(void) {
-	return !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_L_TRIGGER);
+	return leftTriggerEnabled && !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_L_TRIGGER);
 }
 
 /**
  * \return true if Right Bumper Button is being pressed. False otherwise.
  */
 int getRightBumperState(void) {
-	return !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_R_BUMPER);
+	return rightBumperEnabled && !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_R_BUMPER);
 }
 
 /**
  * \return true if Left Bumper Button is being pressed. False otherwise.
  */
 int getLeftBumperState(void) {
-	return !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_L_BUMPER);
+	return leftBumperEnabled && !Chip_GPIO_ReadPortBit(LPC_GPIO, GPIO_L_BUMPER);
 }
 
 /**
